9304d2da569a6e52dbb1e34b4cb0ba1d0f8bb03f
germannrl <50140108+germannrl@users.noreply.github.com>
2021-06-21 16:41:56 +0200

    Delete src/src directory

--- src/src/data/Ontologia.java
+++
@@ -1,9 +0,0 @@
-package src.data;
-
-import java.util.*;
-
-public interface Ontologia {
-
-	public float distance_WP(Long offset1, Long offset2);
-	public List<Long> getHyponymTree(Long offset);
-}
